e31b630ef1a05a865a15fc7e6a135d6de8d9169f
badrikomma <badrikomma00@gmail.com>
2021-03-09 17:13:35 +0530

    initial commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  reset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary m-2 btn-sm" onClick={this.reset}>
+          Reset
+        </button>
+        <br></br>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid navbar-brand">Counters</div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
